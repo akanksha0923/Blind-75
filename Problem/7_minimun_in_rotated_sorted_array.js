0142a370ffff49b7d104c6c11ce59ebc7040b5e4
Akanksha Uchariya <92923699+akanksha0923@users.noreply.github.com>
2025-05-31 20:24:39 +0530

    Create 7_minimun_in_rotated_sorted_array.js

---
+++ Problem/7_minimun_in_rotated_sorted_array.js
@@ -0,0 +1,26 @@
+/*
+  https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+  Input: nums = [3,4,5,1,2]
+  Output: 1
+  Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+  1. Identify the sorted half and eliminate it.
+*/
+
+var findMin = function (nums) {
+    let lowIdx = 0;
+    let highIdx = nums.length - 1;
+    let ans = nums[0];
+    while (lowIdx <= highIdx) {
+        let midIdx = Math.floor((lowIdx + highIdx) / 2);
+        if (nums[lowIdx] <= nums[midIdx]) {
+            ans = Math.min(ans, nums[lowIdx]);
+            lowIdx = midIdx + 1;
+        }
+        else {
+            highIdx = midIdx - 1;
+            ans = Math.min(ans, nums[midIdx])
+        }
+    }
+    return ans;
+};
