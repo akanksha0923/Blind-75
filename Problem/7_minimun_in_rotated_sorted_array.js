9ed3c793cb4c2403fd3c7b0739a067d0982ab52e
Akanksha Uchariya <92923699+akanksha0923@users.noreply.github.com>
2025-06-11 19:25:00 +0530

    rotated

--- Problem/7_minimun_in_rotated_sorted_array.js
+++ Problem/7_minimun_in_rotated_sorted_array.js
@@ -5,7 +5,8 @@
   Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 
   1. Identify the sorted first half and eliminate it.
-*/
+  2. If nums[low] <= nums[mid] which means mid is the part of first sorted half
+  */
 
 var findMin = function (nums) {
     let lowIdx = 0;
