a23306e44981e1603e4f5f8d7aefe5264851fe78
Akanksha Uchariya <92923699+akanksha0923@users.noreply.github.com>
2025-06-11 20:11:17 +0530

    Create 18_hpuse_robber_2.js

---
+++ Problem/18_hpuse_robber_2.js
@@ -0,0 +1,30 @@
+/*https://leetcode.com/problems/house-robber-ii/
+
+run 2 loops
+
+
+*/
+
+var rob = function (nums) {
+    let rob1 = 0
+    let rob2 = 0
+    let max1 = nums[0]
+    let max2 = 0
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        max1 = Math.max(rob1 + nums[i], rob2);
+        rob1 = rob2;
+        rob2 = max1;
+    }
+
+    rob1 = 0;
+    rob2 = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        max2 = Math.max(rob1 + nums[i], rob2);
+        rob1 = rob2;
+        rob2 = max2;
+    }
+
+    return Math.max(max1, max2)
+};
